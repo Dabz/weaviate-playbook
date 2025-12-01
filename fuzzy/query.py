67d60c994e1fe147eaecc22463544874fb3d47c2
Damien Gasparina <damien@gasparina.cloud>
2025-12-01 10:39:36 +0100

    Adding fuzzy example

--- fuzzy/query.py
+++ fuzzy/query.py
@@ -5,25 +5,27 @@ client = weaviate.connect_to_local()
 collection_name = "Companies"
 
 queries = [
-    "P&G",
-    "l'Oréal",
-    "Societe Generale",
-    "Morgan Stanley",
-    "GE",
-    "HSBC",
-    "Goldman Sacks",
-    "3M",
-    "Lorale LLC",
-    "Society General Limited",
-    "Morgan Stannard",
-    "HSB Co.",
-    "Goldman Snacks Co",
+    ("P&G", "P&G"),
+    ("l'Oréal", "l'Oréal"),
+    ("l'Oréa", "l'Oréal"),
+    ("Societe Generale", "Societe Generale"),
+    ("Morgan Stanley", "Morgan Stanley"),
+    ("GE", "GE"),
+    ("HSBC", "HSBC"),
+    ("Goldman Sacks", "Goldman Sacks"),
+    ("3M", "3M"),
+    ("Lorale LLC", "Lorale LLC"),
+    ("Loral LLC", "Lorale LLC"),
+    ("Society General Limited", "Society General Limited"),
+    ("Morgan Stannard", "Morgan Stannard"),
+    ("HSB Co.", "HSB Co."),
+    ("Goldman Snacks Co", "Goldman Snacks Co"),
 ]
 
 collection = client.collections.get(collection_name)
 number_of_error = 0
 
-for query in queries:
+for query, expected_result in queries:
     res = collection.query.bm25(query=query,
                                 query_properties=["name^2", "name_trig"],
                                 limit=1)
@@ -32,7 +34,7 @@ for query in queries:
         res_company = obj.properties["name"]
         break
 
-    if res_company != query:
+    if res_company != expected_result:
         number_of_error += 1
         print("Expecting %s, got %s" % (query, res_company))
 
